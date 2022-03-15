41b7115b4ff44801d4d48d7188a521bb72640dd6
nithyamani <nithya.mn999@gmail.com>
2022-03-15 14:38:20 +0530

    using rest api for marking test status

--- junit-5/src/test/java/tests/LocalTest.java
+++ junit-5/src/test/java/tests/LocalTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
 import runners.WebDriverTest;
 import utils.MarkSessionStatus;
 
@@ -10,20 +12,19 @@ public class LocalTest {
 
     @WebDriverTest
     void localTest(WebDriver driver) {
-        MarkSessionStatus sessionStatus = new MarkSessionStatus();
+        SessionId sessionId = ((RemoteWebDriver) driver).getSessionId();
+        MarkSessionStatus sessionStatus = new MarkSessionStatus(sessionId);
 
         try {
             driver.get("http://localhost:45691/check");
             String validateContent = driver.findElement(By.cssSelector("body")).getText();
             if (validateContent.contains("Up and running")) {
-                sessionStatus.markTestStatus("passed", "Local content validated!", driver);
+                sessionStatus.markTestStatus("passed", "Local content validated!");
+            } else {
+                sessionStatus.markTestStatus("failed", "Local content not validated!");
             }
-            else {
-                sessionStatus.markTestStatus("failed", "Local content not validated!", driver);
-            }
-        }
-        catch (Exception e) {
-            sessionStatus.markTestStatus("failed", "There was some issue!", driver);
+        } catch (Exception e) {
+            sessionStatus.markTestStatus("failed", "There was some issue!");
             System.out.println(e.getMessage());
         }
         driver.quit();
